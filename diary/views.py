4814dbcd12b3680220e250098a507c627d886607
SemiHedge <soorte214@gmail.com>
2022-07-10 16:53:18 +0900

    18 메모 삭제 기능

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from . import db
 from .models import Note
@@ -32,3 +32,22 @@ def home():
             return redirect(url_for('views.home'))
 
     return render_template('home.html')
+
+
+# 메모 삭제 기능
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # POST : 메모 삭제
+    if request.method == "POST":
+        note = request.get_json()
+        note_id = note.get('noteId')
+
+        # db 확인
+        select_note = Note.query.get(note_id)
+        if select_note:
+            if select_note.user_id == current_user.id:
+                db.session.delete(select_note)
+                db.session.commit()
+                return jsonify({'result':True})
+
+        return jsonify({'result':False})
